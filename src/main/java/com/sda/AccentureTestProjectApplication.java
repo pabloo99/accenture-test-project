99fbe44c0a2af3b8feacaa993d7f73bc12fdab20
Paweł Mazur <pawel.mazur@cgm.com>
2024-01-06 15:22:42 +0100

    Pierwszy commit

---
+++ src/main/java/com/sda/AccentureTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccentureTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccentureTestProjectApplication.class, args);
+	}
+
+}
