d31ebe63502b9828763d2786028e4213252606a5
Tomasz <wartego@wp.pl>
2024-01-11 12:54:51 +0100

    added two new movies

--- src/main/java/com/sda/git/MovieService.java
+++ src/main/java/com/sda/git/MovieService.java
@@ -9,6 +9,8 @@ public class MovieService {
         List<Movie> movies = new ArrayList<>();
 
         movies.add(new Movie("Sami swoi", Genre.COMEDY, 1967));
+        movies.add(new Movie("Matrix", Genre.THRILLER, 2004));
+        movies.add(new Movie("Pan Tadeusz", Genre.ACTION, 2001));
 
         return movies;
     }
