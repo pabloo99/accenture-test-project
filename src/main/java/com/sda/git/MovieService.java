c845f8798eebfccaaf2012a2aea6c81bed0febe6
mikolajp2137 <pienkoszmikolaj@gmail.com>
2024-01-11 11:40:20 +0100

    dodano Pana Kleksa

--- src/main/java/com/sda/git/MovieService.java
+++ src/main/java/com/sda/git/MovieService.java
@@ -9,7 +9,7 @@ public class MovieService {
         List<Movie> movies = new ArrayList<>();
 
         movies.add(new Movie("Sami swoi", Genre.COMEDY, 1967));
-
+        movies.add(new Movie("Akademia pana Kleksa", Genre.COMEDY, 2023));
         return movies;
     }
 }
