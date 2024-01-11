82386097c8de0a43c215fd3cd9aa256661217cf8
Aleksandra Fornal <aleksandrafornal00@gmail.com>
2024-01-11 12:38:55 +0100

    Add new movie

--- src/main/java/com/sda/git/MovieService.java
+++ src/main/java/com/sda/git/MovieService.java
@@ -9,6 +9,7 @@ public class MovieService {
         List<Movie> movies = new ArrayList<>();
 
         movies.add(new Movie("Sami swoi", Genre.COMEDY, 1967));
+        movies.add(new Movie("Matrix", Genre.ACTION, 1999));
 
         return movies;
     }
