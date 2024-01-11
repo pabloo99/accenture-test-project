1c2a86ebcb939e1a5efe7bbca85690688ff3bf3b
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2024-01-11 12:53:14 +0100

    add new films and genre

--- src/main/java/com/sda/git/MovieService.java
+++ src/main/java/com/sda/git/MovieService.java
@@ -9,6 +9,8 @@ public class MovieService {
         List<Movie> movies = new ArrayList<>();
 
         movies.add(new Movie("Sami swoi", Genre.COMEDY, 1967));
+        movies.add(new Movie("Mr and Mrs Smith", Genre.ACTION, 1999));
+        movies.add(new Movie("Lord of the rings", Genre.DRAMA, 2010));
 
         return movies;
     }
