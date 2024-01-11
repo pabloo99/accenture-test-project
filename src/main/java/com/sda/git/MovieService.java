6a073c0a4594e61d93849f0caee5a9e4816c35d0
Alex Vl <alexnotami@gmail.com>
2024-01-11 19:25:24 +0100

    Matrix movie added.

--- src/main/java/com/sda/git/MovieService.java
+++ src/main/java/com/sda/git/MovieService.java
@@ -23,6 +23,7 @@ public class MovieService {
         movies.add(new Movie("Inglourious Basterds", Genre.COMEDY, 2009));
         movies.add(new Movie("Akademia pana Kleksa", Genre.COMEDY, 2023));
         movies.add(new Movie("Jakiś film", Genre.COMEDY, 2023));
+        movies.add(new Movie("Matrix 2. Reloaded", Genre.ACTION, 2002));
 
         return movies;
     }
