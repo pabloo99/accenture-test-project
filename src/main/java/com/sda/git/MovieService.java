47e227b9b6a5fcd3ea5b5399f8395a54e4338251
Piotr Szustak <ppszustak@gmail.com>
2024-01-11 12:56:33 +0100

    MovieService - addition of new record

--- src/main/java/com/sda/git/MovieService.java
+++ src/main/java/com/sda/git/MovieService.java
@@ -9,6 +9,7 @@ public class MovieService {
         List<Movie> movies = new ArrayList<>();
 
         movies.add(new Movie("Sami swoi", Genre.COMEDY, 1967));
+        movies.add(new Movie("Szklana pułapka", Genre.ACTION, 1999));
 
         return movies;
     }
