d55afa770b28719417537113904de8165156503c
Agnieszka <agnieszka.ad77@gmail.com>
2024-01-11 13:03:23 +0100

    Movie "Matrix" added

--- src/main/java/com/sda/git/MovieService.java
+++ src/main/java/com/sda/git/MovieService.java
@@ -9,6 +9,7 @@ public class MovieService {
         List<Movie> movies = new ArrayList<>();
 
         movies.add(new Movie("Sami swoi", Genre.COMEDY, 1967));
+        movies.add(new Movie("Matrix", Genre.SCI_FI, 2010));
 
         return movies;
     }
