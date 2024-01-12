a8289c575626681c92fa6a7ed79ccbfb04682751
Artur T Małas <artur.malas25@gmail.com>
2024-01-12 13:40:27 +0100

    Added implementation for departments

---
+++ src/main/java/com/sda/rest/Department.java
@@ -0,0 +1,19 @@
+package com.sda.rest;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "departments")
+@Data
+public class Department {
+
+    @Id
+    private long departmentId;
+
+    private String departmentName;
+    private Integer managerId;
+    private Integer locationId;
+}
---
+++ src/main/java/com/sda/rest/DepartmentController.java
@@ -0,0 +1,42 @@
+package com.sda.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/departments")
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    @Autowired
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @GetMapping
+    public List<Department> getAllDepartments() {
+        return departmentService.getAllDepartments();
+    }
+
+    @PostMapping
+    public void addDepartment(@RequestBody Department department) {
+        departmentService.saveDepartment(department);
+    }
+
+    @PutMapping("/{id}")
+    public void updateDepartment(@PathVariable Long id, @RequestBody Department department) {
+        Department existingDepartment = departmentService.getDepartmentById(id);
+        if (existingDepartment != null) {
+            existingDepartment.setDepartmentId(id);
+            departmentService.updateDepartment(department);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDepartment(@PathVariable Long id) {
+        departmentService.deleteDepartment(id);
+    }
+}
